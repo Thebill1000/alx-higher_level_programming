03c20d1662a8959c41ac58d652f95262429a7d41
thebill1000 <williamsbilljnr100@gmail.com>
2023-01-03 23:54:26 -0800

    7-islower.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,4 +4,4 @@ for num in range(0, 10):
         if num is 8 and num2 is 9:
             print("{:d}{:d}".format(num, num2))
         elif num < num2:
-            print("{:d}{:d}".format(num, num2), end=", "i)
+            print("{:d}{:d}".format(num, num2), end=", ")
