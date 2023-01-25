fd84fa67e1cfbc9e7fab2bc6952ed07435f344f2
thebill1000 <williamsbilljnr100@gmail.com>
2023-01-25 06:10:41 -0800

    blueobjects

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,13 +1,38 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, size=0):
-        if type(size) != int:
-            raise TypeError('size must be an integer')
-        elif size < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = size
+"""Square generation module for Python project 0x06
+"""
 
-    def area(self):
-        size = self.__size
-        return size * size
+
+class Square:
+    """Class defined for square generation.
+
+    Args:
+    __size (int): length of one side of square
+
+    Attributes:
+    __size (int): length of one side of square
+
+    Raises:
+    TypeError: if size is not an integer
+    ValueError: if size is less than 0
+
+    """
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+        def area(self):
+            """Calulates area of square.
+
+            Attributes:
+            __size (int): length of one side of square
+
+            Returns:
+                area (int): length of one side, squared
+
+            """
+            area = self.__size * self.
+            return area
