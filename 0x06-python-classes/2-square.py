4433662045c5df8ce5fd1ba42460b71b7f8cc827
thebill1000 <williamsbilljnr100@gmail.com>
2023-01-25 06:03:12 -0800

    blueobjects

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+"""Square generation module for Python project 0x06
+"""
+
+
 class Square:
     """Defines a square"""
     def __init__(self, size=0):
-        """Initialises the data"""
-        self.__size = size
         if type(size) != int:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
+
+
