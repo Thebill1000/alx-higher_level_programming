914add46ce13a9cedd25ca066598547698460206
thebill1000 <williamsbilljnr100@gmail.com>
2023-03-14 09:44:51 -0700

    sql scripts

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
--- a script that converts a database to UTF-8
+-- a script that converts a database
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE hbtn_0c_0;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
