027a8a413824dac24843920872db793725d9868b
thebill1000 <williamsbilljnr100@gmail.com>
2023-03-15 16:53:02 -0700

    sql queries

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,3 +1,4 @@
 -- a script that creates a database and user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+    GRANT SELECT ON hbtn_0d_2.* 
+    TO 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
