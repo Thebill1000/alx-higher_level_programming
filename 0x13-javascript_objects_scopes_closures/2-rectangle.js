7a4ccd6ddd4643777e1e82ccc734e0c086e41f4e
thebill1000 <williamsbilljnr100@gmail.com>
2023-04-12 02:22:00 -0700

    JS objects

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/* a class rectangle that defines a rectangle */
+module.exports = class Rectangle {
+	constructor (w, h) {
+		this.width = w;
+		this.height = h;
+		if (w > 0 && h > 0) {
+		}
+	}
+};
